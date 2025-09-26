4ea8b4f7d6d7e693434b856e2d00368f6c393ce6
Daniel Tummler <daniel.tummler.rosales@hackaboss.com>
2025-09-26 10:34:21 +0200

    Reapply "back"
    This reverts commit 6f3addda9caac79db9dbbef68d736060e6aad3e0.

--- overview_.py
+++ overview_.py
@@ -124,7 +124,6 @@ def grafica_veredicto_vertical(df_filtered, df_dicts):
 # Rol y Veredicto
 def grafica_veredicto_rol(df_filtered, df_dicts):
 
-
     df_func = pd.merge(left = df_filtered, right = df_dicts["Trabajadores"].explode("Nuevo puesto"), left_on = "id", right_on = "id", how = "left")
     
     df_rol_veredicto = pd.merge(left = df_func.groupby(by = ["Nuevo puesto", "Veredicto"], as_index = False).agg({"id" : "count"}),
@@ -173,3 +172,4 @@ def quinta_grafica(df_filtered, df_dicts):
 
     fig.update_layout(yaxis_range=[0, 1])
     return fig
+    
